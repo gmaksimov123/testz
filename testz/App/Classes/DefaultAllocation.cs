a4fcdc8979cc556e680bc594ef2c97ff4a3369f4
German Maksimov <g.maksimov@retailinform.ru>
2019-01-10 13:42:06 +0500

    класс распределитель члена массива по массиву

---
+++ testz/App/Classes/DefaultAllocation.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using testz.App.Interfaces;
+
+namespace testz.App.Classes
+{
+    public class DefaultAllocation : IAllocation
+    {
+        public DefaultAllocation()
+        {
+        }
+
+        public int[] Run(int[] array)
+        {
+            int allocationIndex = FindMaxIndex(array);
+            int allocationValue = array[allocationIndex];
+            array[allocationIndex] = 0;
+
+            int index = allocationIndex + 1;
+            int lastIndex = array.Length - 1;
+
+            while (allocationValue > 0)
+            {
+                if (index > lastIndex)
+                {
+                    index = 0;
+                }
+                array[index]++;
+                allocationValue--;
+                index++;
+            }
+            return array;
+        }
+
+        protected int FindMaxIndex(int[] array)
+        {
+            int maxIndex = 0;
+            int lastIndex = array.Length;
+            for (int i = 0; i <= lastIndex; i++)
+            {
+                if (array[maxIndex] < array[i])
+                {
+                    maxIndex = i;
+                }
+            }
+            return maxIndex;
+        }
+    }
+}
---
+++ testz/App/Interfaces/IAllocation.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testz.App.Interfaces
+{
+    public interface IAllocation
+    {
+        int[] Run(int[] array, int allocationPosition);
+    }
+}
